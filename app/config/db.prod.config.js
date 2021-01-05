7722b14d005fc9a2b6e5cb5471b31ea2d2b1a717
Radovan Čapek <capek.radovan@gmail.com>
2021-01-05 22:52:29 +0100

    db config

---
+++ app/config/db.prod.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    HOST: "ec2-54-217-213-79.eu-west-1.compute.amazonaws.com",
+    USER: "epjojnwhzydeqn",
+    PASSWORD: "91d6d149f296f643576dafd336cf03e12e7a224d9a2cc0048f75c76687494a42",
+    DB: "d63a3utd5huilu",
+    dialect: "postgres",
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+};
